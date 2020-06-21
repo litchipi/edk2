c0dfb40dea3e81114ac48dcc3d0b45a1e8bc6cb9
Achin Gupta <achin.gupta@arm.com>
2020-06-21 18:33:34 +0100

    ArmPkg/IndustryStandard: Add barebones FF-A header
    This patch adds a rudimentary header file with defines for FF-A ABIs that will
    be used as the transport between S-EL0 and the SPM
    
    Signed-off-by: Achin Gupta <achin.gupta@arm.com>

---
+++ ArmPkg/Include/IndustryStandard/ArmFfaSvc.h
@@ -0,0 +1,16 @@
+/** @file
+*
+*  Copyright (c) 2020, ARM Limited. All rights reserved.
+*
+*  SPDX-License-Identifier: BSD-2-Clause-Patent
+*
+**/
+
+#ifndef __ARM_FFA_SVC_H__
+#define __ARM_FFA_SVC_H__
+
+#define ARM_SVC_ID_FFA_VERSION_AARCH32             	0x84000063
+#define ARM_SVC_ID_FFA_MSG_SEND_DIRECT_REQ_AARCH64	0xC400006F
+#define ARM_SVC_ID_FFA_MSG_SEND_DIRECT_RESP_AARCH64	0xC4000070
+
+#endif
